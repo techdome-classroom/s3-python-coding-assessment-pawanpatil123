28cf2954c3b6e6cf8c27fe0185dfd08a7160d671
piyush patil <piyushpatil44099@gmail.com>
2024-09-30 16:09:22 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -15,12 +15,11 @@ class Solution(object):
             if char in mapping:
                 top_element = stack.pop() if stack else '#'
                 
-                # The mapping for the closing bracket doesn't match the top element of the stack
+                
                 if mapping[char] != top_element:
                     return False
             else:
-                # It's an opening bracket, push onto the stack
+                
                 stack.append(char)
         
-        # If the stack is empty, all the brackets are matched
         return not stack
