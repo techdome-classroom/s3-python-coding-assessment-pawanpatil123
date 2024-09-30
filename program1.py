f4835d6f21e93260a09fcc0c25c1a467cd9078fa
piyush patil <piyushpatil44099@gmail.com>
2024-09-30 16:08:16 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,33 +1,13 @@
 class Solution(object):
     def isValid(self, s):
-        
+        """
+        :type s: str
+        :rtype: bool
+        """
         pass
 
 
-class Solution:
-    def isValid(self, s: str) -> bool:
-        stack = []
-        mapping = {
-            ')': '(',
-            '}': '{',
-            ']': '['
-        }
-        
-        for char in s:
-            # If the character is a closing bracket
-            if char in mapping:
-                # Pop the topmost element from the stack if it exists, else assign a dummy value
-                top_element = stack.pop() if stack else '#'
-                
-                # The mapping for the closing bracket doesn't match the top element of the stack
-                if mapping[char] != top_element:
-                    return False
-            else:
-                # It's an opening bracket, push onto the stack
-                stack.append(char)
-        
-        # If the stack is empty, all the brackets are matched
-        return not stack
+
 
 
 
