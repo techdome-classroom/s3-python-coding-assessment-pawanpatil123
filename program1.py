e705fad2b11580d60c406724b549b6f2ff73ef05
piyush patil <piyushpatil44099@gmail.com>
2024-09-30 16:09:12 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,17 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
-
-
-
-
-
-
-
-    
-
-
-
-  
-
+        stack = []
+        mapping = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+        
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                
+                # The mapping for the closing bracket doesn't match the top element of the stack
+                if mapping[char] != top_element:
+                    return False
+            else:
+                # It's an opening bracket, push onto the stack
+                stack.append(char)
+        
+        # If the stack is empty, all the brackets are matched
+        return not stack
