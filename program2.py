37b2a8871a0ec5b60e0192710adc64bbd0a43825
piyush patil <piyushpatil44099@gmail.com>
2024-09-30 16:13:59 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,7 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
-
-
-
+        roman_to_int = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+            # If current numeral is smaller than the next one, subtract it
+            if i < n - 1 and roman_to_int[s[i]] < roman_to_int[s[i + 1]]:
+                total -= roman_to_int[s[i]]
+            else:
+                # Otherwise, add the numeral's value
+                total += roman_to_int[s[i]]
+        
+        return total
